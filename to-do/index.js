16eeb4cb79407ecabad094a7516708f986147f9d
Tan <tanmoy.pto@gmail.com>
2017-07-25 13:27:43 +0530

    added quit function with hotkeys

--- to-do/index.js
+++ to-do/index.js
@@ -1,11 +1,49 @@
-const electron = require ('electron')
-const { app, BrowserWindow, ipcMain } = electron;
+const electron = require('electron')
+
+//Menu menu menu
+const { app, BrowserWindow, ipcMain, Menu } = electron;
 
 let mainWindow;
 
-app.on('ready',()=>{
+app.on('ready', () => {
     mainWindow = new BrowserWindow({});
-    mainWindow.loadURL(`file://${__dirname}/index.html`);
+    mainWindow.loadURL(`file://${__dirname}/main.html`);
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate); //build the menu. May change with different windows
+    Menu.setApplicationMenu(mainMenu)//actually set/show the menu into the window. This replaces the default menu adn the functionalities
     console.log('App ready');
 });
+
+//the following template will be used inside the ready function as a parameter to buildFromTemplate function
+const menuTemplate = [
+    //{} This empty object is for mac compatibilty
+    {
+        label: 'File',
+        submenu: [
+            { label: "New ToDo" },
+            {
+                label: "Quit",
+                //hotkey iife
+                accelerator: (()=>{
+                    if(process.platform === 'darwin'){
+                        return 'Command + Q';
+                    } else {
+                        return 'Ctrl+Q';
+                    }
+                })(),
+                //quit function
+                click(){
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+//condition for cross platform check
+//adds an empty object to menuTemplate if on macos
+if (process.platform === 'darwin') {
+    menuTemplate.unshift({})
+}
+
 
