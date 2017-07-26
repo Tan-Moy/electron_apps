49b55009e4e9a9b76910fdfd5cc4325394ca72c9
Tan <tanmoy.pto@gmail.com>
2017-07-26 14:07:16 +0530

    added developer tools back to EA2

--- to-do/index.js
+++ to-do/index.js
@@ -4,15 +4,28 @@ const electron = require('electron')
 const { app, BrowserWindow, ipcMain, Menu } = electron;
 
 let mainWindow;
+let addWindow;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({});
-    mainWindow.loadURL(`file://${__dirname}/main.html`);
+    mainWindow.loadURL(`file://${__dirname}/main.html`);//load the html file
+    mainWindow.on('closed', () => app.quit());//closes everything including the child windows
+
 
     const mainMenu = Menu.buildFromTemplate(menuTemplate); //build the menu. May change with different windows
     Menu.setApplicationMenu(mainMenu)//actually set/show the menu into the window. This replaces the default menu adn the functionalities
     console.log('App ready');
 });
+
+//creating window to add todos
+function createAddWindow() {
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: "Add new Todo"
+    });
+    addWindow.loadURL(`file://${__dirname}/add.html`);
+};
 
 //the following template will be used inside the ready function as a parameter to buildFromTemplate function
 const menuTemplate = [
@@ -20,19 +33,24 @@ const menuTemplate = [
     {
         label: 'File',
         submenu: [
-            { label: "New ToDo" },
+            {
+                label: "New ToDo",
+                click() {
+                    createAddWindow();
+                }
+            },
             {
                 label: "Quit",
                 //hotkey iife
-                accelerator: (()=>{
-                    if(process.platform === 'darwin'){
+                accelerator: (() => {
+                    if (process.platform === 'darwin') {
                         return 'Command + Q';
                     } else {
                         return 'Ctrl+Q';
                     }
                 })(),
                 //quit function
-                click(){
+                click() {
                     app.quit();
                 }
             }
@@ -44,6 +62,21 @@ const menuTemplate = [
 //adds an empty object to menuTemplate if on macos
 if (process.platform === 'darwin') {
     menuTemplate.unshift({})
+}
+
+//check if the app is running in dev or prod
+if(process.env.NODE_ENV !== 'production'){
+    menuTemplate.push({
+        label: 'Developer',
+        submenu:[{
+            label:'Toggle Developer Tools',
+            accelerator: process.platform === 'darwin' ? 'command+alt+i' : 'ctrl+shift+i',
+            //function to show the developer tools for the focused window only
+            click(item,focusedWindow){
+                focusedWindow.toggleDevTools();
+            }
+        }]
+    })
 }
 
 
