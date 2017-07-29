11dbeb5c37f6fc66ecbf1beb37016bd76b3e5290
Tan <tanmoy.pto@gmail.com>
2017-07-29 12:35:45 +0530

    Communication between React and Electron enabled and background activity ensured

--- tasky/app/main_window.js
+++ tasky/app/main_window.js
@@ -1,16 +1,23 @@
 const electron = require('electron');
 const { BrowserWindow } = electron;
 
-class MainWindow extends BrowserWindow{
-    constructor(){
+class MainWindow extends BrowserWindow {
+    constructor() {
         super({
-        height: 500,
-        width: 300,
-        frame: false, //removes the statusbar
-        resizable: false,
-        show: false
-    });
+            height: 500,
+            width: 300,
+            frame: false, //removes the statusbar
+            resizable: false,
+            show: false,
+            webPreferences: { backgroundThrottling: false }//must be there so that the app can run in background
+        });
 
+        this.on('blur', this.onBlur.bind(this));
+    }
+
+    //handling in and out of focus
+    onBlur() {
+        this.hide();
     }
 }
 
--- tasky/index.js
+++ tasky/index.js
@@ -1,22 +1,24 @@
-const electron = require('electron');
-const TimerTray = require('./app/timer_tray')
-const { app, BrowserWindow, Tray } = electron;
 const path = require('path');//a node module that takes care of crossplatform pathing :-)
+const TimerTray = require('./app/timer_tray')
+const MainWindow = require('./app/main_window')
+const electron = require('electron');
+
+const { app, ipcMain } = electron;
 
 let mainWindow;
 let tray;
 
 app.on('ready', () => {
     //app.dock.hide();//hide dock icon if relevant most probably mac
-    mainWindow = new BrowserWindow();
+    mainWindow = new MainWindow();
     mainWindow.loadURL(`file://${__dirname}/src/index.html`);
-
-    //handling in and out of focus
-    mainWindow.on('blur',()=>{
-        mainWindow.hide();
-    })
 
     const iconName = process.platform === 'win32' ? 'windows-icon@2x.png' : 'iconTemplate.png'
     const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
     tray = new TimerTray(iconPath, mainWindow); //the parameter is the path to the tray icon
 });
+
+ipcMain.on('update-timer',(event,timeLeft)=>{
+    console.log("bello");
+    tray.setTitle(timeLeft); //works only on macos
+})
